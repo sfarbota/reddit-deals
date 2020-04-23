e7bdb020b45a82c1f89cc016c33d247977d87d57
Farbota <Steven.Farbota@coleparmer.com>
2020-04-23 10:30:15 -0700

    Move from @material-ui to react-bootstrap.

--- src/components/ImageButton.jsx
+++ src/components/ImageButton.jsx
@@ -1,8 +1,6 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import ButtonBase from "@material-ui/core/ButtonBase";
-import Typography from "@material-ui/core/Typography";
-// import {clothes, pc_parts, video_games} from '../images';
+// TODO: import {clothes, pc_parts, video_games} from '../images';
+import { Image, Container, Row, Col } from "react-bootstrap";
 
 const images = [
   {
@@ -87,120 +85,34 @@ const images = [
   },
 ];
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    flexWrap: "wrap",
-    minWidth: 300,
-    width: "100%",
-  },
-  image: {
-    position: "relative",
-    height: 200,
-    [theme.breakpoints.down("xs")]: {
-      width: "100% !important", // Overrides inline-style
-      height: 100,
-    },
-    "&:hover, &$focusVisible": {
-      zIndex: 1,
-      "& $imageBackdrop": {
-        opacity: 0.15,
-      },
-      "& $imageMarked": {
-        opacity: 0,
-      },
-      "& $imageTitle": {
-        border: "4px solid currentColor",
-      },
-    },
-  },
-  focusVisible: {},
-  imageButton: {
-    position: "absolute",
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    color: theme.palette.common.white,
-  },
-  imageSrc: {
-    position: "absolute",
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
-    backgroundSize: "cover",
-    backgroundPosition: "center 40%",
-  },
-  imageBackdrop: {
-    position: "absolute",
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
-    backgroundColor: theme.palette.common.black,
-    opacity: 0.4,
-    transition: theme.transitions.create("opacity"),
-  },
-  imageTitle: {
-    position: "relative",
-    padding: `${theme.spacing(2)}px ${theme.spacing(4)}px ${
-      theme.spacing(1) + 6
-    }px`,
-  },
-  imageMarked: {
-    height: 3,
-    width: 18,
-    backgroundColor: theme.palette.common.white,
-    position: "absolute",
-    bottom: -2,
-    left: "calc(50% - 9px)",
-    transition: theme.transitions.create("opacity"),
-  },
-}));
-
 export default function ButtonBases(props) {
-  const classes = useStyles();
-
   return (
-    <div className={classes.root}>
-      {images.map((image) => (
-        <ButtonBase
-          focusRipple
-          onClick={() => {
-            console.log("Clicked: " + image.title);
-            props.changeUrl(image.title);
-          }}
-          key={image.title}
-          className={classes.image}
-          focusVisibleClassName={classes.focusVisible}
-          style={{
-            width: image.width,
-          }}
-        >
-          <span
-            className={classes.imageSrc}
-            style={{
-              backgroundImage: `url(${image.url})`,
-            }}
-          />
-          <span className={classes.imageBackdrop} />
-          <span className={classes.imageButton}>
-            <Typography
-              component="span"
-              variant="subtitle1"
-              color="inherit"
-              className={classes.imageTitle}
+    <Container>
+      <Row noGutters>
+        {images.map((image) => (
+          <Col xs={6} md={4} lg={3}>
+            <a
+              className="image-button-link"
+              href="#"
+              onClick={() => {
+                console.log("Clicked: " + image.title);
+                props.changeUrl(image.title);
+              }}
             >
-              {image.title}
-              <span className={classes.imageMarked} />
-            </Typography>
-          </span>
-        </ButtonBase>
-      ))}
-    </div>
+              <div
+                className="image-button"
+                style={{
+                  backgroundImage:
+                    //"linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2))," +
+                    " url('" + image.url + "')",
+                }}
+              >
+                <span className="image-button-text">{image.title}</span>
+              </div>
+            </a>
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 }
