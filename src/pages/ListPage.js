360eae3e9188eae262ba62346708381df25dba38
Farbota <Steven.Farbota@coleparmer.com>
2020-06-08 02:12:15 -0700

    Remove "deals" from list view header

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { useParams } from 'react-router-dom';
-import { Context } from '../context';
-import Loading from '../components/Loading';
-import { getRedditDeals } from '../utils/dataApi';
-import Deal from '../components/Deal';
-import DefImg from '../images/baseline_add_shopping_cart_black.png';
-import '../App.css';
+import React, { useState, useEffect, useContext } from "react";
+import { useParams } from "react-router-dom";
+import { Context } from "../context";
+import Loading from "../components/Loading";
+import { getRedditDeals } from "../utils/dataApi";
+import Deal from "../components/Deal";
+import DefImg from "../images/baseline_add_shopping_cart_black.png";
+import "../App.css";
 
 function DealList() {
   const { subreddit } = useParams();
@@ -16,10 +16,10 @@ function DealList() {
   useEffect(() => {
     setLoading(true);
     getRedditDeals(subreddit)
-      .then(res => res.data.children)
-      .then(res => {
+      .then((res) => res.data.children)
+      .then((res) => {
         const posts = [];
-        res.map(item => {
+        res.map((item) => {
           posts.push(item.data);
           return posts;
         });
@@ -29,7 +29,9 @@ function DealList() {
   }, [subreddit, setState]);
 
   const filteredDeals = () => {
-    const result = deals.filter(deal => deal.link_flair_text === '[Deal/Sale]');
+    const result = deals.filter(
+      (deal) => deal.link_flair_text === "[Deal/Sale]"
+    );
     return result;
   };
 
@@ -43,14 +45,14 @@ function DealList() {
     return (
       <div class="list-group list-group-flush">
         <h2 className="mt-4 mb-4 d-flex justify-content-center text-success">
-          {subreddit + ' deals'}
+          {subreddit}
         </h2>
         {deals.map((deal, index) => {
           let img;
           if (
             !deal.thumbnail ||
-            deal.thumbnail === 'self' ||
-            deal.thumbnail === 'default'
+            deal.thumbnail === "self" ||
+            deal.thumbnail === "default"
           ) {
             img = DefImg;
           } else img = deal.thumbnail;
